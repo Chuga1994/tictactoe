fdfb0f5dca76800107897ac834ef96444f701cfb
Evgeny <95447157+chugachuga94@users.noreply.github.com>
2022-02-22 00:55:35 +0300

    load project

---
+++ ticTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ticTacToe
+//
+//  Created by Евгений Чугуевец on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
