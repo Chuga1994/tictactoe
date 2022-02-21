1869c18690ccd7c934f87d058f641f89d412e4b5
Evgeny <95447157+chugachuga94@users.noreply.github.com>
2022-02-22 01:41:01 +0300

    add buttons and touch func

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum Turn {
+        case Nought
+        case Cross
+    }
+    
+    
+    
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    var NOUGHT = "O"
+    var CROSS = "X"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func boardTapAction(_ sender: UIButton) {
+    }
+    func addBoard(_ sender: UIButton) {
+        if(sender.title(for: .normal) == nil) {
+            if(currentTurn == Turn.Nought) {
+                sender.setTitle(NOUGHT, for: .normal)
+                turnLabel.text = CROSS
+            }
+        }
+    }
 
 }
 
